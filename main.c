d847f13cbcd419aa52f0f6fcf1841fbda8065a1c
BOT-Mojjo <mojjo.olander@gmail.com>
2024-02-16 10:44:15 +0100

    mesh_rotation optimisation, render output improvements, bug fixes

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 #define ifnt(condition) if (!condition)
 #define otherwise else if
 
-#define H_RESOLUTION 80
-#define V_RESOLUTION 25
+#define H_RESOLUTION 180
+#define V_RESOLUTION 60
 // expanded grayscale
 #define BRIGHTNESS(depth) "$@B%%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."[depth >= 0 ? (depth < 69 ? depth : 68) : 0]
 // reversed grayscale
@@ -165,9 +165,8 @@ int ray_collision(triangle tri, vec3 ray_orig, vec3 ray_vec, vec3 *uv_out)
 }
 
 // returns new triangle to not mutate orignal, does mutate originals normal vector
-triangle t_rotate(triangle *tri, double x_rad, double y_rad, double z_rad)
+void mesh_rotate(triangle **mesh_in, triangle *mesh_out, double x_rad, double y_rad, double z_rad, int mesh_size)
 {
-    triangle result;
     // precalculate the cosine and sine of the radians
     double cos_x, cos_y, cos_z, sin_x, sin_y, sin_z;
     cos_x = cos(x_rad);
@@ -193,26 +192,25 @@ triangle t_rotate(triangle *tri, double x_rad, double y_rad, double z_rad)
     z3 = cos_x * cos_y;
 
     // Multplication
-    for (char i = 0; i < 3; i++)
+    for (int ii = 0; ii < mesh_size; ii++)
     {
-        vec3 point = tri->verts[i];
+        for (char i = 0; i < 3; i++)
+        {
+            mesh_out[ii].verts[i].x = x1 * (*mesh_in + ii)->verts[i].x + x2 * (*mesh_in + ii)->verts[i].y + x3 * (*mesh_in + ii)->verts[i].z;
+            mesh_out[ii].verts[i].y = y1 * (*mesh_in + ii)->verts[i].x + y2 * (*mesh_in + ii)->verts[i].y + y3 * (*mesh_in + ii)->verts[i].z;
+            mesh_out[ii].verts[i].z = z1 * (*mesh_in + ii)->verts[i].x + z2 * (*mesh_in + ii)->verts[i].y + z3 * (*mesh_in + ii)->verts[i].z;
+        }
 
-        result.verts[i].x = x1 * point.x + x2 * point.y + x3 * point.z;
-        result.verts[i].y = y1 * point.x + y2 * point.y + y3 * point.z;
-        result.verts[i].z = z1 * point.x + z2 * point.y + z3 * point.z;
+        vec3 e0 = sub(mesh_out[ii].verts[1], mesh_out[ii].verts[0]);
+        vec3 e1 = sub(mesh_out[ii].verts[2], mesh_out[ii].verts[0]);
+        mesh_out[ii].normal = cross(e1, e0);
     }
-
-    vec3 e0 = sub(result.verts[1], result.verts[0]);
-    vec3 e1 = sub(result.verts[2], result.verts[0]);
-    tri->normal = cross(e1, e0);
-    return result;
 }
 
 int load_obj(char *path, triangle **mesh_out, short *mesh_size)
 {
     short vec_amount = 0, tri_amount = 0;
     FILE *file = fopen(path, "r");
-
     if (file == NULL)
         return 0;
 
@@ -241,7 +239,6 @@ int load_obj(char *path, triangle **mesh_out, short *mesh_size)
         return 0;
     *mesh_size = tri_amount;
     vec_amount = 0, tri_amount = 0;
-    long unsigned long test = 0;
     while (fgets(str_buffer, 128, file))
     {
         if (str_buffer[0] == 'v' && str_buffer[1] == ' ')
@@ -267,38 +264,40 @@ int load_obj(char *path, triangle **mesh_out, short *mesh_size)
                 {
                     cur_char++;
                 }
-                tri.verts[i] = vec_buffer[atoi(&str_buffer[cur_char + 1])];
+                tri.verts[i] = vec_buffer[atoi(&str_buffer[cur_char + 1]) - 1];
                 cur_char++;
             }
             *(*mesh_out + tri_amount) = tri;
             tri_amount++;
         }
-        test++;
     }
     fclose(file);
     return 1;
 }
 
 #include "shapes.c" // cube, tetrahedron
-char input;
 int main()
 {
-    // set_raw_term();
+    set_raw_term();
     short poly_count;
     triangle *mesh = 0;
+    char path[] = "./intercepter.obj";
 
-    load_obj("./teapot.obj", &mesh, &poly_count);
+    printf("Loading object %s\n", path);
+    load_obj(path, &mesh, &poly_count);
+    printf("Object %s loaded succesfully\n", path);
 
     double h_step = FOV / (double)H_RESOLUTION;
     double h_start = -h_step * (H_RESOLUTION / 2);
     double v_step = FOV / (double)V_RESOLUTION;
     double v_start = -v_step * (V_RESOLUTION / 2);
-    char buffer[H_RESOLUTION + 1];
+    char buffer[V_RESOLUTION][H_RESOLUTION + 1];
 
     for (int i = 0; i < poly_count * 3; i++)
     {
-        mesh[i / 3].verts[i % 3] = scale(mesh[i / 3].verts[i % 3], 1.5);
+        mesh[i / 3].verts[i % 3] = scale(mesh[i / 3].verts[i % 3], 5);
     }
+    triangle render_mesh[poly_count];
 
     vec3 light_vec3 = (vec3){0.5, 0, -0.5};
     vec3 rotation;
@@ -312,6 +311,9 @@ int main()
         vec3 temp_coords;
         char hit;
         double light_level;
+
+        mesh_rotate(&mesh, render_mesh, 0 * iii, 0.05 * iii, 0 * iii, poly_count);
+
         for (int ii = 0; ii < V_RESOLUTION; ii++)
         {
             for (int i = 0; i < H_RESOLUTION; i++)
@@ -321,27 +323,29 @@ int main()
                 for (short iV = 0; iV < poly_count; iV++)
                 { // rotate the input on render-step to reduce
                   // accumalitive deformation of mesh from rotation
-                    if (!ray_collision(t_rotate(&mesh[iV], 0.01 * iii, 0.01 * iii, 0 * iii), (vec3){0, 0, 20}, (vec3){(h_start + (i * h_step)) * 1.4, v_start + (ii * v_step), -1}, &temp_coords))
+                    if (!ray_collision(render_mesh[iV], (vec3){0, 0, 12}, (vec3){(h_start + (i * h_step)) * 1.4, v_start + (ii * v_step), -1}, &temp_coords))
                         continue;
                     if (temp_coords.z > depth)
                         continue;
                     depth = temp_coords.z;
-                    light_level = acos(dot(light_vec3, mesh[iV].normal) / (mag(light_vec3) * mag(mesh[iV].normal)));
-                    buffer[i] = BRIGHTNESS((int)floor((light_level * 57.3) / 1.4));
+                    light_level = acos(dot(light_vec3, render_mesh[iV].normal) / (mag(light_vec3) * mag(render_mesh[iV].normal)));
+                    buffer[ii][i] = BRIGHTNESS((int)floor((light_level * 57.3) / 1.4));
                     hit = 1;
                 }
                 if (!hit)
-                    buffer[i] = ' ';
+                    buffer[ii][i] = ' ';
             }
-            buffer[H_RESOLUTION] = 0;
-            printf("%s\n", buffer);
+            buffer[ii][H_RESOLUTION] = 0;
         }
         clock_t end = clock() - start;
-        // usleep(16666 - ((int)end * 10));
+        for (int i = V_RESOLUTION-1; i >= 0 ; i--)
+        {
+            printf("%s\n", buffer[i]);
+        }
+        if ((int)end < 16666)
+            usleep(16666 - end);
         iii++;
     }
-    printf("%d\n", poly_count);
-    printf("%f, %f, %f\n", mesh[0].verts[0].x, mesh[0].verts[0].y, mesh[0].verts[0].z);
     free(mesh);
     return 0;
 }
